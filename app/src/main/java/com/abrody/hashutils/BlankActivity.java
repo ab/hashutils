bf887f8000ebe91e2165f3f8b842e051e73f5bca
Andy Brody <git@abrody.com>
2015-10-22 23:24:20 -0700

    WIP

--- app/src/main/java/com/abrody/hashutils/BlankActivity.java
+++ app/src/main/java/com/abrody/hashutils/BlankActivity.java
@@ -36,7 +36,9 @@ public class BlankActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+        String digest = HashLib.sha1sum(message);
+        intent.putExtra(EXTRA_MESSAGE, digest);
         startActivity(intent);
     }
 
