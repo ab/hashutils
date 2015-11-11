4719fad53a17efb689f26264a6fd681ca98a9b6b
Andy Brody <git@abrody.com>
2015-11-11 01:05:33 -0800

    Add pretty damn fully functional TEXT hashing.

--- app/src/main/java/com/abrody/hashutils/HomeActivity.java
+++ app/src/main/java/com/abrody/hashutils/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.abrody.hashutils;
 
+import android.app.ActionBar;
 import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
@@ -13,8 +15,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,13 +28,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity {
@@ -53,6 +61,9 @@ public class HomeActivity extends AppCompatActivity {
     public static final String PREFS_NAME = "HashUtilsPrefs";
 
     private Set<String> hashAlgorithms;
+
+    private static final LayoutParams matchWidth_wrapHeight = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+    private static final LayoutParams wrapWidth_matchHeight = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
 
     public List<String> getAllHashAlgoriths() {
         List<String> list = new ArrayList<>(HashLib.supportedAlgorithms);
@@ -89,8 +100,8 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        super.onStop();
         saveHashAlgoPreferences();
+        super.onStop();
     }
 
     @Override
@@ -238,27 +249,137 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_home_multifile, container, false);
+            View v = inflater.inflate(R.layout.fragment_home_multifile, container, false);
+            ((HomeActivity) v.getContext()).textInitializeResultsLayout();
+            return v;
         }
     }
 
     public void textGenerateHash(View view) {
         Log.d(TAG, "textGenerateHash()");
-        EditText et_digest = (EditText) findViewById(R.id.home_text_hexdigest);
+
         EditText et_text = (EditText) findViewById(R.id.home_edit_text);
-        String digest = HashLib.sha1sum(et_text.getText().toString());
+        String text = et_text.getText().toString();
+
+        /*
+        EditText et_digest = (EditText) findViewById(R.id.home_text_hexdigest);
+        String digest = HashLib.sha1sum(text);
         et_digest.setText(digest);
+        */
+
+
+        LinearLayout container = (LinearLayout) findViewById(R.id.home_text_results);
+        container.removeAllViews();
+
+        for (String algo : getHashAlgorithmsSorted()) {
+            addHashResultView(container, algo, text);
+        }
+    }
+
+    public void textInitializeResultsLayout() {
+        LinearLayout container = (LinearLayout) findViewById(R.id.home_text_results);
+
+        if (container.getChildAt(0).getId() != R.id.home_text_results_placeholder) {
+            Log.d(TAG, "textInitializeResultsLayout(): init already performed");
+            return;
+        }
+
+        Log.d(TAG, "textInitializeResultsLayout()");
+
+        container.removeAllViews();
+
+        for (String algo : getHashAlgorithmsSorted()) {
+            addHashResultView(container, algo, null);
+        }
+    }
+
+    private int dpAsPixels(int dp) {
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    private void addHashResultView(LinearLayout container, String algorithm, String text) {
+        Log.d(TAG, String.format("addHashResultView(%s)", algorithm));
+
+        // create LinearLayout to contain label and digest
+        LinearLayout resultLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams resultLayoutParams = new LinearLayout.LayoutParams(matchWidth_wrapHeight);
+        resultLayoutParams.topMargin = dpAsPixels(6);
+        resultLayoutParams.bottomMargin = dpAsPixels(6);
+        resultLayout.setLayoutParams(resultLayoutParams);
+        resultLayout.setOrientation(LinearLayout.HORIZONTAL);
+        resultLayout.setBackgroundResource(R.drawable.myrect);
+//        View.setElevation(resultLayout, dpAsPixels(2));
+        ViewCompat.setElevation(resultLayout, dpAsPixels(2));
+
+        container.addView(resultLayout);
+
+        // create algorithm label
+        TextView label = new TextView(this);
+        label.setLayoutParams(wrapWidth_matchHeight);
+        label.setText(algorithm);
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        label.setGravity(Gravity.CENTER);
+        label.setPadding(dpAsPixels(4), 0, dpAsPixels(4), 0);
+        resultLayout.addView(label);
+
+        // create edittext for digest result
+        EditText digest = new EditText(this);
+        digest.setLayoutParams(matchWidth_wrapHeight);
+        // this didn't work: digest.setInputType(InputType.TYPE_NULL);
+        digest.setKeyListener(null);
+        digest.setTextIsSelectable(true);
+        resultLayout.addView(digest);
+
+        if (text != null) {
+            digest.setText(HashLib.hexDigest(algorithm, text));
+        }
+
+        /*
+   <LinearLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            android:orientation="horizontal"
+            android:elevation="2dp"
+            android:background="@drawable/myrect"
+            android:layout_marginTop="16dp"
+            >
+            <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="match_parent"
+                android:text="SHA1"
+                android:textSize="20sp"
+                android:gravity="center"
+                android:paddingLeft="4sp"
+                android:paddingRight="4sp"
+                tools:ignore="HardcodedText" />
+
+            <EditText android:id="@+id/home_text_hexdigest"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"
+
+                android:inputType="none"
+                android:textIsSelectable="true"
+                />
+         */
+
+
     }
 
     public static final String ALGO_PREFS = "EnabledAlgorithms";
 
     private void loadHashAlgoPreferences() {
-        ArrayList<String> list = new ArrayList<String>();
-
-
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        hashAlgorithms = settings.getStringSet(ALGO_PREFS, defaultHashAlgoPreferences());
-        Log.d(TAG, "Loaded algo preferences: " + TextUtils.join(",", hashAlgorithms));
+        Set<String> saved = settings.getStringSet(ALGO_PREFS, null);
+        if (saved == null) {
+            hashAlgorithms = defaultHashAlgoPreferences();
+            Log.d(TAG, "Using default algo preferences: " + TextUtils.join(",", hashAlgorithms));
+
+        } else {
+            // copy saved object to avoid mutating it
+            hashAlgorithms = new HashSet<String>(saved);
+            Log.d(TAG, "Loaded algo preferences: " + TextUtils.join(",", hashAlgorithms));
+        }
     }
 
     private void saveHashAlgoPreferences() {
@@ -267,11 +388,54 @@ public class HomeActivity extends AppCompatActivity {
         // All objects are from android.context.Context
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
-        editor.putStringSet(ALGO_PREFS, hashAlgorithms);
-        editor.apply();
+        editor.putStringSet(ALGO_PREFS, hashAlgorithms).apply();
     }
 
     private Set<String> defaultHashAlgoPreferences() {
         return new HashSet<String>(Arrays.asList("SHA1", "SHA256"));
+    }
+
+    private void DEBUG_PREFLIST(String ... prefs) { // TODO XXX
+
+            // Define default return values. These should not display, but are needed
+            final String STRING_ERROR = "error!";
+            final Integer INT_ERROR = -1;
+            // ...
+            final Set<String> SET_ERROR = new HashSet<>(1);
+
+            // Add an item to the set
+            SET_ERROR.add("Set Error!");
+
+            // Loop through the Shared Prefs
+            Log.i("Loading Shared Prefs", "-----------------------------------");
+            Log.i("------------------", "-------------------------------------");
+
+            for (String pref_name: prefs) {
+
+                SharedPreferences preference = getSharedPreferences(pref_name, MODE_PRIVATE);
+                Map<String, ?> prefMap = preference.getAll();
+
+                Object prefObj;
+                Object prefValue = null;
+
+                for (String key : prefMap.keySet()) {
+
+                    prefObj = prefMap.get(key);
+
+                    if (prefObj instanceof String) prefValue = preference.getString(key, STRING_ERROR);
+                    if (prefObj instanceof Integer) prefValue = preference.getInt(key, INT_ERROR);
+                    // ...
+                    if (prefObj instanceof Set) prefValue = preference.getStringSet(key, SET_ERROR);
+
+                    Log.i(String.format("Shared Preference : %s - %s", pref_name, key),
+                            String.valueOf(prefValue));
+
+                }
+
+                Log.i("------------------", "-------------------------------------");
+
+            }
+
+            Log.i("Loaded Shared Prefs", "------------------------------------");
     }
 }
